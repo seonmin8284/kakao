d115db699327175c5f458836171f7da047c01b33
seonmin8284 <seonmin8284@naver.com>
2025-06-05 19:04:47 +0900

    new file:   .dockerignore 	new file:   fly.toml 	modified:   main.py

--- main.py
+++ main.py
@@ -414,3 +414,10 @@ async def kakao_webhook(request: KakaoRequest):
         }
     }
     return JSONResponse(content=response_json)
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    
+    port = int(os.getenv("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
